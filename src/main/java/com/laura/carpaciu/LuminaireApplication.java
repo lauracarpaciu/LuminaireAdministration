d92aa33fe19164748fcec6c4ba28b5aa4860d61d
Mirela <lauracarpaciu>
2022-04-03 10:30:09 +0300

    update Application class

--- src/main/java/com/laura/carpaciu/LuminaireApplication.java
+++ src/main/java/com/laura/carpaciu/LuminaireApplication.java
@@ -2,8 +2,14 @@ package com.laura.carpaciu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@Import({SecurityConfig.class, PdfGeneratorConfig.class, FieldsValidationConfig.class})
+@EnableAspectJAutoProxy
+@EntityScan
+@ComponentScan
 public class LuminaireApplication {
 	 public static void main(String[] args) {
 		    SpringApplication.run(LuminaireApplication.class, args);

13b85a54eea2266d82cd9872e320c72fea52fa56
Mirela <lauracarpaciu>
2022-04-03 18:35:44 +0300

    generated constructor for Exception

--- src/main/java/com/laura/carpaciu/errors/luminaire/InvalidPriceException.java
+++ src/main/java/com/laura/carpaciu/errors/luminaire/InvalidPriceException.java
@@ -1,5 +1,16 @@
 package com.laura.carpaciu.errors.luminaire;
 
-public class InvalidPriceException extends Exception {
+public class InvalidPriceException extends RuntimeException {
+
+	public InvalidPriceException() {
+		
+	}
+
+	public InvalidPriceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }

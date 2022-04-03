c081a320eabce6d145d9fec581b88141a528f10f
Mirela <lauracarpaciu>
2022-04-03 10:27:46 +0300

    create an Application Class

---
+++ src/main/java/com/laura/carpaciu/LuminaireApplication.java
@@ -0,0 +1,12 @@
+package com.laura.carpaciu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaireApplication {
+	 public static void main(String[] args) {
+		    SpringApplication.run(LuminaireApplication.class, args);
+		  }
+
+}

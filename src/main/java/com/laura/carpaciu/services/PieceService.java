9ba65a632ae9b470526e5f9da5ba776c81360072
Mirela <lauracarpaciu>
2022-04-09 13:19:26 +0300

    update interface

--- src/main/java/com/laura/carpaciu/services/PieceService.java
+++ src/main/java/com/laura/carpaciu/services/PieceService.java
@@ -1,23 +1,16 @@
 package com.laura.carpaciu.services;
 
-
 import org.springframework.transaction.annotation.Transactional;
 
 import com.laura.carpaciu.entity.luminaire.Piece;
 
 public interface PieceService {
-//	@Transactional
-//	void addPart(Piece part);
-//
-//	@Transactional
-//	Piece findPartByPartNumber(String partNumber);
-//
-//	@Transactional
-//	int decreasePartCount(int decrement, String partNumber);
-
+	@Transactional
 	void addPiece(Piece part);
 
+	@Transactional
 	Piece findPieceByPartNumber(String partNumber);
 
+	@Transactional
 	int decreasePieceCount(int decrement, String partNumber);
 }

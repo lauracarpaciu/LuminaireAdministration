d37cd4823943fd014c4f4a233a2745a7b290f389
Mirela <lauracarpaciu>
2022-10-01 15:47:56 +0300

    Refactor Order Controller

--- src/main/java/com/laura/carpaciu/controllers/order/OrderController.java
+++ src/main/java/com/laura/carpaciu/controllers/order/OrderController.java
@@ -1,31 +1,29 @@
 package com.laura.carpaciu.controllers.order;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import com.laura.carpaciu.cache.MiniCache;
-import com.laura.carpaciu.entity.clients.Company;
-import com.laura.carpaciu.entity.clients.Person;
-import com.laura.carpaciu.entity.luminaire.Luminaire;
-import com.laura.carpaciu.entity.order.LuminaireCases;
+import com.laura.carpaciu.dto.ServiceOrderIdAndStatusDto;
+import com.laura.carpaciu.entity.order.PieceOrder;
 import com.laura.carpaciu.entity.order.ServiceOrder;
-import com.laura.carpaciu.entity.user.User;
-import com.laura.carpaciu.services.OrderService;
-import com.laura.carpaciu.services.UserService;
+import com.laura.carpaciu.entity.order.WorkOrder;
 
 import lombok.AllArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
-@RequestMapping("/serviceOrder")
 public class OrderController {
 
 	private final OrderService serviceOrderService;
@@ -42,71 +40,66 @@ public class OrderController {
 		this.miniCache = miniCache;
 	}
 
-	@GetMapping("/searchLuminaire")
-	public String searchLuminaireByVin(HttpServletRequest request, Model model) {
-
-		String serialNumber = request.getParameter("serialNumber");
-		Luminaire luminaire = miniCache.findLuminaireBySerialNumber(serialNumber);
-		model.addAttribute("luminaire", luminaire);
-
-		return "redirect:/serviceOrder/serviceOrder";
+	@RequestMapping(value = "/orders", method = RequestMethod.POST)
+	public ResponseEntity<?> createServiceOrder(ServiceOrder serviceOrder) throws Exception {
+		return Optional.ofNullable(serviceOrderService.createServiceOrder(serviceOrder))
+				.map(a -> new ResponseEntity<Employee>(a, HttpStatus.OK)).orElseThrow(() -> new Exception("Not found"));
 	}
 
-	@GetMapping("/findPerson")
-	public String searchPersonByCnp(HttpServletRequest request, Model model) {
-
-		String cnp = request.getParameter("cnp");
-		Person person = miniCache.findPersonByCnp(cnp);
-		model.addAttribute("person", person);
-		miniCache.resetCompanySearch();
-
-		return "redirect:/serviceOrder/serviceOrder";
-
+	@RequestMapping(value = "/orders", method = RequestMethod.GET)
+	public ResponseEntity<?> findAllServiceOrder() throws Exception {
+		return Optional.ofNullable(serviceOrderService.findAllServiceOrder())
+				.map(a -> new ResponseEntity<Set<ServiceOrder>>(a, HttpStatus.OK))
+				.orElseThrow(() -> new Exception("Not found"));
 	}
 
-	@GetMapping("/findCompany")
-	public String searchCompany(HttpServletRequest request, Model model) {
-
-		String cui = request.getParameter("cui");
-		Company company = miniCache.findCompanyByCui(cui);
-		model.addAttribute("company", company);
-		miniCache.resetPersonSearch();
-
-		return "redirect:/serviceOrder/serviceOrder";
+	@RequestMapping(value = "/orders", method = RequestMethod.GET)
+	public ResponseEntity<?> allServiceOrderIdAndStatus() throws Exception {
+		return Optional.ofNullable(serviceOrderService.allServiceOrderIdAndStatus())
+				.map(a -> new ResponseEntity<List<ServiceOrderIdAndStatusDto>>(a, HttpStatus.OK))
+				.orElseThrow(() -> new Exception("Not found"));
 	}
 
-	@PostMapping("/luminaireProblems")
-	public String createServiceOrder(HttpServletRequest request) {
-
-		if (miniCache.retrieveLuminaire().getId() != 0
-				&& ((miniCache.retrievePerson().getId() != 0) || (miniCache.retrieveCompany().getId() != 0))) {
-
-			String luminaireProblems = request.getParameter("luminaireProblems");
-
-			String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-
-			User user = userService.findUseByUsername(username);
-			LuminaireCases problems = new LuminaireCases();
-			problems.setProblems(luminaireProblems);
-
-			ServiceOrder serviceOrder = new ServiceOrder.Builder().withLuminaireProblems(problems)
-					.withLuminaire(miniCache.retrieveLuminaire()).withUser(user).build();
-
-			if (miniCache.retrievePerson().getId() == 0) {
-				serviceOrder.setClient(miniCache.retrieveCompany());
-			}
-
-			if (miniCache.retrieveCompany().getId() == 0) {
-				serviceOrder.setClient(miniCache.retrievePerson());
-			}
-
-			serviceOrderService.createServiceOrder(serviceOrder);
-
-			miniCache.resetCarSearch();
-			miniCache.resetPersonSearch();
-			miniCache.resetCompanySearch();
-		}
-
-		return "redirect:/serviceOrder/serviceOrder";
+	@RequestMapping(value = "/orders", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateServiceOrder(ServiceOrder serviceOrder, int decrement, String partNumber)
+			throws Exception {
+		return Optional.ofNullable(serviceOrderService.updateServiceOrder(serviceOrder, decrement, partNumber))
+				.map(a -> new ResponseEntity<ServiceOrder>(a, HttpStatus.OK))
+				.orElseThrow(() -> new Exception("Not found"));
 	}
+
+	@RequestMapping(value = "/orders", method = RequestMethod.GET)
+	public ResponseEntity<?> findServiceOrderParts(Long id) throws Exception {
+		return Optional.ofNullable(serviceOrderService.findServiceOrderParts(id))
+				.map(a -> new ResponseEntity<ServiceOrder>(a, HttpStatus.OK))
+				.orElseThrow(() -> new Exception("Not found"));
+	}
+
+	@RequestMapping(value = "/orders", method = RequestMethod.GET)
+	public ResponseEntity<?> getPartsFormServiceOrder(Long id) throws Exception {
+		return Optional.ofNullable(serviceOrderService.getPartsFormServiceOrder(id))
+				.map(a -> new ResponseEntity<List<PieceOrder>>(a, HttpStatus.OK))
+				.orElseThrow(() -> new Exception("Not found"));
+	}
+
+	@RequestMapping(value = "/orders", method = RequestMethod.GET)
+	public ResponseEntity<?> findCompleteServiceOrderById(Long id) throws Exception {
+		return Optional.ofNullable(serviceOrderService.findCompleteServiceOrderById(id))
+				.map(a -> new ResponseEntity<List<PieceOrder>>(HttpStatus.OK))
+				.orElseThrow(() -> new Exception("Not found"));
+	}
+
+	@RequestMapping(value = "/orders", method = RequestMethod.GET)
+	public ResponseEntity<?> closeOrder(ServiceOrder serviceOrder) throws Exception {
+		return Optional.ofNullable(serviceOrderService.closeOrder(serviceOrder))
+				.map(a -> new ResponseEntity<int>(a, HttpStatus.OK)).orElseThrow(() -> new Exception("Not found"));
+	}
+
+	@RequestMapping(value = "/orders", method = RequestMethod.GET)
+	public ResponseEntity<?> findAllWorksInOrder(Long id) throws Exception {
+		return Optional.ofNullable(serviceOrderService.findAllWorksInOrder(id))
+				.map(a -> new ResponseEntity<List<WorkOrder>>(a, HttpStatus.OK))
+				.orElseThrow(() -> new Exception("Not found"));
+	}
+
 }
